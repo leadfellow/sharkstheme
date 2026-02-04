3a9e20c172e8c3c7cba685011c4f68fbf6f9bc9a
root <admin@leadfellow.com>
2026-02-04 11:53:46 +0200

    ads

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Theme version
-define('SHARKS_VERSION', '1.8.1');
+define('SHARKS_VERSION', '1.8.3');
 
 // Theme directory path
 define('SHARKS_DIR', get_stylesheet_directory());

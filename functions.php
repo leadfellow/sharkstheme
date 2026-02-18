9dbc9798d5614fd56309259b03c3d9a28acff210
root <admin@leadfellow.com>
2026-02-18 13:39:24 +0200

    mobile

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Theme version
-define('SHARKS_VERSION', '2.4.2');
+define('SHARKS_VERSION', '2.4.3');
 
 // Theme directory path
 define('SHARKS_DIR', get_stylesheet_directory());

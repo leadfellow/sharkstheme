6357a37af06ec5d316e030c3ff944a1f65e3b0c9
root <admin@leadfellow.com>
2026-02-20 12:37:54 +0200

    koduleht

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Theme version
-define('SHARKS_VERSION', '2.5.1');
+define('SHARKS_VERSION', '2.5.2');
 
 // Theme directory path
 define('SHARKS_DIR', get_stylesheet_directory());

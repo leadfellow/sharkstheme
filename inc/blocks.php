1c817ccd570a73cd0a77a1294235734de84c033c
root <admin@leadfellow.com>
2026-02-21 15:05:55 +0200

    roller

--- inc/blocks.php
+++ inc/blocks.php
@@ -1199,8 +1199,9 @@ add_action('acf/init', function() {
             wp_enqueue_style(
                 'roll-process-css',
                 get_template_directory_uri() . '/assets/css/30-components/roll-process.css',
-                [],
-                filemtime(get_template_directory() . '/assets/css/30-components/roll-process.css')
+                ['site-css'],
+                filemtime(get_template_directory() . '/assets/css/30-components/roll-process.css'),
+                'all'
             );
             wp_enqueue_script(
                 'roll-process-js',
--- template-parts/blocks/roll-process/roll-process.php
+++ template-parts/blocks/roll-process/roll-process.php
@@ -37,41 +37,26 @@ if (!$main_title && !$process_items) {
 }
 ?>
 
-<style>
-/* Critical CSS loaded inline to prevent FOUC */
-.block-roll-process .roll-process__content > * {
-    margin: 0 !important;
-    padding: 0 !important;
-}
-.block-roll-process .roll-process__content > * + * {
-    margin-top: 20px !important;
-}
-.block-roll-process .roll-process__container {
-    gap: 70px !important;
-}
-</style>
-
-<section id="<?php echo esc_attr($anchor); ?>" class="<?php echo esc_attr($class_name . $align_class); ?>" style="background-color: <?php echo esc_attr($background_color); ?> !important; margin: 0 !important; padding: 0 !important;">
-    <div class="roll-process__container" style="padding: 120px 58px !important; gap: 70px !important;">
+<section id="<?php echo esc_attr($anchor); ?>" class="<?php echo esc_attr($class_name . $align_class); ?>" style="background-color: <?php echo esc_attr($background_color); ?>;">
+    <div class="roll-process__container">
         
         <?php if ($main_title): ?>
-            <h2 class="roll-process__main-title" style="margin: 0 !important;"><?php echo esc_html($main_title); ?></h2>
+            <h2 class="roll-process__main-title"><?php echo esc_html($main_title); ?></h2>
         <?php endif; ?>
         
-        <div class="roll-process__content" style="gap: 0 !important;">
+        <div class="roll-process__content">
             <?php if ($process_items): ?>
                 <?php foreach ($process_items as $index => $item): 
                     $text = $item['text'] ?? '';
                     $style = $item['style'] ?? 'gray';
                     $text_class = ($style === 'black') ? 'roll-process__text--black' : 'roll-process__text--gray';
-                    $margin_style = ($index === 0) ? 'margin: 0 !important;' : 'margin: 20px 0 0 0 !important;';
                 ?>
-                    <div class="roll-process__divider" style="<?php echo $margin_style; ?>"></div>
-                    <p class="roll-process__text <?php echo esc_attr($text_class); ?>" style="margin: 20px 0 0 0 !important;" data-original="<?php echo esc_attr($text); ?>">
+                    <div class="roll-process__divider"></div>
+                    <p class="roll-process__text <?php echo esc_attr($text_class); ?>" data-original="<?php echo esc_attr($text); ?>">
                         <?php echo esc_html($text); ?>
                     </p>
                 <?php endforeach; ?>
-                <div class="roll-process__divider" style="margin: 20px 0 0 0 !important;"></div>
+                <div class="roll-process__divider"></div>
             <?php endif; ?>
             
             <?php if ($show_cursor_icon): ?>

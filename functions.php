b6f542a8d2a03b62c23615458e61e10b8d937eae
root <admin@leadfellow.com>
2026-02-21 12:41:41 +0200

    2e

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Theme version
-define('SHARKS_VERSION', '2.5.8');
+define('SHARKS_VERSION', '2.5.9');
 
 // Theme directory path
 define('SHARKS_DIR', get_stylesheet_directory());

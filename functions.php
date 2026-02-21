da88edefc78ad8e9477bdf2cb8a8fca564235fd0
root <admin@leadfellow.com>
2026-02-21 14:18:08 +0200

    roller

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Theme version
-define('SHARKS_VERSION', '2.8.3');
+define('SHARKS_VERSION', '2.8.4');
 
 // Theme directory path
 define('SHARKS_DIR', get_stylesheet_directory());
